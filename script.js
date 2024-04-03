b32955dadc1b573763740b7167fb5df8ef8c4242
Devansh Aggarwal <Devanshaggarwal23@gmail.com>
2024-04-03 18:22:58 +0530

    added events

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const buy = document.querySelector(".buy");
+
+const contact = document.querySelector("#contact");
+const feedback = document.querySelector("#feedback");
+const partner = document.querySelector("#partner");
+
+buy.addEventListener("click", ()=>{
+    console.log("Buy is clicked");
+});
+
+contact.addEventListener("click", ()=>{
+    console.log("Contact is clicked");
+});
+
+feedback.addEventListener("click", ()=>{
+    console.log("Feedback is clicked");
+});
+
+partner.addEventListener("click", ()=>{
+    console.log("Partner with Us is clicked");
+});
+
+
